9acae89115d42d850aab312b5c25aa4fc89f9a7f
Mikrovatt <viktorov.sergey@inbox.ru>
2022-02-16 21:44:57 +0300

    install django

--- etc/init.sh
+++ etc/init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+source /home/user/my_app/my_venv/bin/activate
 cd /home/box/web/
-sudo gunicorn -b 0.0.0.0:8080 hello:app
+gunicorn -b 0.0.0.0:8080 hello:app
